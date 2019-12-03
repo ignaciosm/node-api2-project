98ac4bfefa18a6980ddf4e2a78748c6d62270b72
ignaciosm <ignaciosm@gmail.com>
2019-12-03 15:51:23 -0500

    server up and running

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// const hubsRouter = require('../hubs/hubs-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Posts API</h>
+    <p>Welcome to the Lambda Posts API</p>
+  `);
+});
+
+// server.use('/api/hubs', hubsRouter);
+
+// add an endpoint that returns all the messages for a hub
+// add an endpoint for adding new message to a hub
+
+module.exports = server;
